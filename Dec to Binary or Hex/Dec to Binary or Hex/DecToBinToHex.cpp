c22555e31b242014931f405c502c525ee81ec729
JT Gulick <jtgulick1@gmail.com>
2023-10-14 15:20:39 -0400

    updated conversion code

--- Dec to Binary or Hex/Dec to Binary or Hex/DecToBinToHex.cpp
+++ Dec to Binary or Hex/Dec to Binary or Hex/DecToBinToHex.cpp
@@ -1,30 +1,51 @@
 #include <iostream>
+#include <string>
+
 using namespace std;
 
 void DecToBin(int n) {
-    int binaryNum[32];
-
+    int rem;
+    string bin = "";
     int i = 0;
     while (n > 0) {
-
-        binaryNum[i] = n % 2;
+        rem = n % 2;
+        bin += to_string(rem);
         n = n / 2;
         i++;
     }
+    reverse(bin.begin(), bin.end());
+    cout << bin;
+    cout << endl;
+}
 
-    for (int j = i - 1; j >= 0; j--)
-        std::cout << binaryNum[j];
+void BinToDec(int n)
+{
+    int zed = 0;
+    int base = 1;
+    while (n != 0) {
+        int rem = n % 10;
+        n = n / 10;
+        zed += rem * base;
+        base = base * 2;
+    }
+    cout << zed << endl;
 }
 
 void DecToHex(int n) {
-    cout << "0x" << std::hex << n << endl;
+    cout << "0x" << hex << n << endl;
+}
+
+void HexToDex(int h) {
+    cout << dec << h << endl;
 }
 
 int main() {
-	int dec = 34;
-    cin >> dec;
+	int dec = 503;
+    float h = 0x255f;
+    int b = 111110111;
 	DecToBin(dec);
-    cout << endl;
+    BinToDec(b);
 	DecToHex(dec);
+    HexToDex(h);
 	return 0;
 }
